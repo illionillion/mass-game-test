f05d36f69130e7f0a720710393fe0ae2b1cd1dcd
yuto ichinose <yuto.0526.sano@gmail.com>
2022-08-10 09:32:46 +0900

    Object.freezあまり意味なかった？？

--- js/main.js
+++ js/main.js
@@ -4,18 +4,21 @@ import {Player} from "./lib/player.js";
 import {P} from "./lib/action.js";
 
 (function(){
-    let tables=new Table();
+    const tables=new Table();
     tables.create("#screen");
-    let player=new Player(tables);
-    let $=new P(player,tables);
+    const player=new Player(tables);
+    const $=new P(player,tables);
+    // Object.freeze($)
+    // Object.freeze($.Turn)
+    // Object.freeze($.Frontstep)
     // $.Frontstep();
     document.querySelector("#run-btn").addEventListener("click",function(){
-        // (function(){
+        (function(){
       
         eval(document.querySelector("#editor").value);
         // $.call(document.querySelector("#editor").value);
 
-        // }())
+        }())
         $.actionId=0;
     
     })
